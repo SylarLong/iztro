aafc40a93ae3bc0543f05ee43bf18ba7e00a44c2
Sylar <alsogood@qq.com>
2023-07-27 22:30:29 +0800

    chore: init project

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # astro
 astrolabe generator of The Purple Star Astrology (Zi Wei Dou Shu). 
+
+## Requirement
+
+- node: v14.15.1+
---
+++ src/__tests__/index.test.ts
@@ -0,0 +1,7 @@
+import { greeter } from '../index';
+
+describe('Astrolabe', () => {
+  test('Greeter', () => {
+    expect(greeter()).toBe('你好，我是一个紫微斗数排盘开源库，欢迎使用～');
+  });
+});
