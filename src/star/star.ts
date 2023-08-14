d0ae607fb0adc0150dd2ad8d8997d7abb4b54e5a
Long-Ping <long.ping@navercorp.com>
2023-08-14 11:33:33 +0800

    chore: 完善注释

--- src/star/star.ts
+++ src/star/star.ts
@@ -102,6 +102,14 @@ export const setSecondaryStar = (solarDateStr: string, timeIndex: number, fixLea
   return stars;
 };
 
+/**
+ * 安杂耀
+ *
+ * @param solarDateStr 阳历日期字符串
+ * @param timeIndex 时辰索引【0～12】
+ * @param fixLeap 是否修复闰月，假如当月不是闰月则不生效
+ * @returns 38杂耀
+ */
 export const getOtherStar = (solarDateStr: string, timeIndex: number, fixLeap?: boolean) => {
   const stars = initStars();
   const { yearly } = getHeavenlyStemAndEarthlyBranchBySolarDate(solarDateStr, timeIndex);
