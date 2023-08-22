0b76452edf8f25c9e5f0444a07b4fb016cca0ac9
Sylar <alsogood@qq.com>
2023-08-23 00:07:11 +0800

    🧹 update docs

---
+++ docs/assets/quick-start.md.98165c43.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,o as a,c as n,S as l}from"./chunks/framework.3dda5700.js";const A=JSON.parse('{"title":"ASTRO","description":"","frontmatter":{},"headers":[],"relativePath":"quick-start.md","filePath":"quick-start.md"}'),p={name:"quick-start.md"},o=l("",32),t=[o];function e(c,r,y,D,F,C){return a(),n("div",null,t)}const E=s(p,[["render",e]]);export{A as __pageData,E as default};
