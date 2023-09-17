805c9d6aa49380dc45929a703b0248dc4aee793d
Sylar <alsogood@qq.com>
2023-09-18 00:08:30 +0800

    📃 update docs

--- docs/assets/learn_setup.md.4a3fe8bd.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,o as e,c as a,X as s}from"./chunks/framework.85943196.js";const u=JSON.parse('{"title":"紫微斗数安星诀","description":"","frontmatter":{},"headers":[],"relativePath":"learn/setup.md","filePath":"learn/setup.md"}'),o={name:"learn/setup.md"},r=s("",3),c=[r];function _(n,l,i,p,d,h){return e(),a("div",null,c)}const f=t(o,[["render",_]]);export{u as __pageData,f as default};
